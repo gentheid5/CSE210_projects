7d7c8b9aca45a8ea137e399549e336ade0ee3254
Gentry Heider <hei22004@byui.edu>
2023-09-16 17:27:08 -0600

    prep 2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please input your grade percentage. ");
+        string inputPercentage = Console.ReadLine();
+        int percentage = int.Parse(inputPercentage);
+
+        //variable for grade
+        string grade;
+
+        // grade percentage if statement chain
+        if (percentage > 89)
+        {
+            grade = "A";
+        }
+
+        else if (percentage > 79)
+        {
+            grade = "B";
+        }
+        else if (percentage > 69)
+        {
+            grade = "C";
+        }
+        else if (percentage > 59)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}.");
+
+        if (percentage > 69)
+        {
+            Console.WriteLine("Congrats, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass, but don't give up!");
+        }
+
     }
 }
