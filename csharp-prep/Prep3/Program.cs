67111cafb34186f86081b052b54c1919320959d9
Gentry Heider <hei22004@byui.edu>
2023-09-23 17:44:31 -0600

    prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // random number generator 
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        // random number set point outside of magic number range
+        int numberGuess = -1;
+
+        // while loop for guessing
+        while (numberGuess != magicNumber){
+
+            Console.Write("What is your guess? ");
+            numberGuess = int.Parse(Console.ReadLine());
+
+            if (numberGuess < magicNumber){
+                Console.WriteLine("Higher");
+            }
+            else if (numberGuess > magicNumber){
+                Console.WriteLine("Lower");
+            }
+            else{
+                Console.WriteLine("Correct!");
+            }
+        }
+    
     }
 }
