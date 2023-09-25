a632897e970ab0749445b4dbabb434f487512071
Gentry Heider <hei22004@byui.edu>
2023-09-24 20:54:05 -0600

    prep 4 assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+    // Creating a new list
+    List<int> numbers = new List<int>();
+
+    Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+    
+    int userNumber = -1;
+
+
+    while (userNumber != 0){
+        Console.Write("Enter number: ");
+        userNumber = int.Parse(Console.ReadLine());
+
+        numbers.Add(userNumber);
+
+
+
+    }
+    // Add up the sum of the numbers
+    int sum = 0;
+    foreach (int number in numbers){
+        sum += number;
+    }
+    Console.WriteLine($"The Sum is: {sum}");
+
+        
+    // average of numbers
+    float average = ((float)sum) / numbers.Count;
+    Console.WriteLine($"The avergae is: {average}");
+
+    
+    //Largest number Finder
+    int largestNumber = numbers[0];
+    foreach (int number in numbers){
+        if (number > largestNumber){
+            largestNumber = number;
+        }
+
+    }
+
+    Console.WriteLine($"The largest number: {largestNumber}");
+
     }
 }
